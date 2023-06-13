ca25352a69ef851e5625746bd76f619d8fff8cb6
Alex <53791286+henriksound-bit@users.noreply.github.com>
2023-06-12 21:46:42 -0700

    Add remaining routes to index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const userRoutes = require('./user.routes');
+const assignmentRoutes = require('./assignment.routes');
+const submissionRoutes = require('./submission.routes');
+const courseRoutes = require('./course.routes');
+
 
 const router = express.Router();
 
 router.use('/users', userRoutes);
+router.use('/assignments', assignmentRoutes);
+router.use('/submissions', submissionRoutes);
+router.use('/courses', courseRoutes);
 
-// Other routers here
 
 module.exports = router;
