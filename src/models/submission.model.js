614984815a497e46b4343250dc941fb8596effa7
Nathan-Hausman <hausmann@oregonstate.edu>
2023-06-15 14:38:34 -0700

    Fixed timestamp type

--- src/models/submission.model.js
+++ src/models/submission.model.js
@@ -4,7 +4,7 @@ const sequelize = require("../lib/sequelize")
 
 
 const Submission = sequelize.define("submission", {
-    timestamp: { type: DataTypes.TIME, allowNull: false },
+    timestamp: { type: DataTypes.DATE, allowNull: false },
     grade: { type: DataTypes.FLOAT, allowNull: true },
     file: { type: DataTypes.STRING, allowNull: false }
 })
