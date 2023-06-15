10d80504aeeddaa1934ae531a2ee7193a728511a
Alex <53791286+henriksound-bit@users.noreply.github.com>
2023-06-14 17:12:31 -0700

    Moved 2 functions from assignment.controller.js that should have been in submission.controller.js

--- src/controllers/submission.controller.js
+++ src/controllers/submission.controller.js
@@ -4,6 +4,12 @@ const { Submission, SubmissionClientFields } = require("../models/submission.mod
 
 const router = Router()
 
+/* TODO /assignments/{id}/submissions Post
+ * Create and store a new Assignment with specified data
+ * and adds it to the application's database.
+ * Only an authenticated User with 'student' role who is enrolled in
+ * the Course corresponding to the Assignment's `courseId` can create a Submission.
+ */
 async function createSubmission (req, res, next) {
     try {
         const submission = await Submission.create(
@@ -69,9 +75,67 @@ async function deleteSubmission (req, res, next) {
     }
 }
 
+/* TODO /assignments/{id}/submissions Get
+ * Returns the list of all Submissions for an Assignment. This list should be paginated.
+ * Only an authenticated User with 'admin' role or an authenticated 'instructor' User
+ * whose ID matches the `instructorId` of the Course corresponding to the Assignment's `courseId`
+ * can fetch the Submissions for an Assignment.
+ * */
+async function getAllSubmissions(req, res, next) {
+    //TODO Add authentication check
+
+    let page = parseInt(req.query.page) || 1
+    page = page < 1 ? 1 : page
+    const numPerPage = 10
+    const offset = (page - 1) * numPerPage
+    const assignmentId = req.params.assignmentId
+
+    try {
+        const result = await Submission.findAndCountAll({
+            where: {
+                assignmentId: assignmentId
+            },
+            limit: numPerPage,
+            offset: offset
+        })
+
+        /*
+         * Generate HATEOAS links for surrounding pages.
+         */
+        const lastPage = Math.ceil(result.count / numPerPage)
+        const links = {}
+        //TODO check if :assignmentId works right here
+        if (page < lastPage) {
+            links.nextPage = `/:assignmentId/submissions?page=${page + 1}`
+            links.lastPage = `/businesses?page=${lastPage}`
+        }
+        if (page > 1) {
+            links.prevPage = `/:assignmentId/submissions?page=${page - 1}`
+            links.firstPage = "/:assignmentId/submissions?page=1"
+        }
+
+        /*
+         * Construct and send response.
+         */
+        res.status(200).json({
+            submissions: result.rows,
+            pageNumber: page,
+            totalPages: lastPage,
+            pageSize: numPerPage,
+            totalCount: result.count,
+            links: links
+        })
+    } catch (e) {
+        next(e)
+    }
+}
+
+
+
 module.exports = {
     createSubmission,
     getSubmission,
     updateSubmission,
-    deleteSubmission
+    deleteSubmission,
+    getAllSubmissions
 }
